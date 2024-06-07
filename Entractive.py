19ffbcd28f822fc3cf5372d8b4fd6ae18769cfe4
saketh-crystalwg <saketh@crystalwg.com>
2024-06-07 01:31:17 -0400

    RND addition

--- Entractive.py
+++ Entractive.py
@@ -33,7 +33,23 @@ select customer_fk, max(date(c_date)) as 30_dpst_date  from platform.customer_tr
 where status in ('APPROVED','SUCCESSFUL')  \
 and trx_type = 'DEPOSIT' \
 and DATEDIFF(SYSDATE(),date(c_date)) <= 30 \
-group by 1) \
+group by 1), \
+ \
+RND as (select b.name as brand_name, a.id as customer_id, date(a.last_logged_activity_date) as last_log_date,  \
+c.accept_marketing_offer_sms as sms_consent, c.accept_marketing_offer as email_consent, a.email, \
+c.referral_info, c.first_name, c.last_name, a.login as user_name, c.phone, c.city,  d.country_iso_code, date(a.c_date) as registration_date \
+from platform.customers as a  \
+left join platform.merchants as b \
+on a.merchant_fk = b.id \
+left join platform.customer_attributes as  c \
+on a.id = c.customer_fk \
+left join platform.countries as d  \
+on a.country_fk = d.id \
+where a.country_fk in (75) \
+and DATEDIFF(SYSDATE(),a.c_date) <= 120 and DATEDIFF(SYSDATE(),a.c_date) >  7 \
+and a.id not in (select distinct customer_fk from platform.customer_transactions where status in ('APPROVED','SUCCESSFUL') \
+and trx_type = 'DEPOSIT' )  \
+)  \
  \
 select c.name as brand_name, a.customer_fk as customer_id, a.referral_info, a.first_name, a.last_name, \
 b.login as user_name, email, a.phone,  city, d.country_iso_code, date(b.c_date) as registration_date, last_dpst_date, \
@@ -45,12 +61,23 @@ left join platform.merchants as c \
 on b.merchant_fk = c.id \
 left join platform.countries as d \
 on b.country_fk = d.id \
-inner join txn_base as e \
+inner join txn_base as e  \
 on e.customer_fk = a.customer_fk \
 left join 30_days_txn_base  as f \
 on a.customer_fk = f.customer_fk \
 where d.id = 75 \
-and f.30_dpst_date is null",con = connection)
+and f.30_dpst_date is null  \
+ \
+union all  \
+ \
+select brand_name, customer_id,referral_info,first_name,last_name, user_name, \
+email, phone, city, country_iso_code, registration_date, \
+''  as last_dpst_date, email_consent,sms_consent from RND",con = connection)
+
+Entractive_2["email"] = Entractive_2["email"].apply(lambda x: x.decode('utf-8'))
+
+Entractive_2["user_name"] = Entractive_2["user_name"].apply(lambda x: x.decode('utf-8'))
+
 
 Entractive_2['brand_name'] = ['PLAYDINGO' if x == 'PDIN' \
                                   else x for x in Entractive_2['brand_name']]
@@ -70,8 +97,22 @@ Entractive_1 = pd.read_sql_query("with txn_base  as  ( \
 select customer_fk, max(date(c_date)) as last_dpst_date  from platform.customer_transactions ct \
 where status in ('APPROVED','SUCCESSFUL') \
 and trx_type = 'DEPOSIT' \
-group by 1) \
+group by 1), \
  \
+RND as (select b.name as brand_name, a.id as customer_id, date(a.last_logged_activity_date) as last_log_date, \
+c.accept_marketing_offer_sms as sms_consent, c.accept_marketing_offer as email_consent, a.email \
+from platform.customers as a \
+left join platform.merchants as b \
+on a.merchant_fk = b.id \
+left join platform.customer_attributes as  c \
+on a.id = c.customer_fk \
+where a.country_fk in (75) \
+and DATEDIFF(SYSDATE(),a.c_date) <= 120 and DATEDIFF(SYSDATE(),a.c_date) >  7 \
+and a.id not in (select distinct customer_fk from platform.customer_transactions where status in ('APPROVED','SUCCESSFUL') \
+and trx_type = 'DEPOSIT' ) \
+) \
+\
+\
 select c.name as brand_name, a.customer_fk as customer_id, d.last_dpst_date, date(b.last_logged_activity_date) as last_log_date, \
 a.accept_marketing_offer_sms as sms_consent, a.accept_marketing_offer as email_consent, b.email \
 from  platform.customer_attributes as a \
@@ -81,7 +122,13 @@ left join platform.merchants as c \
 on b.merchant_fk = c.id \
 inner join txn_base as d \
 on a.customer_fk = d.customer_fk \
-where b.country_fk = 75", con = connection)
+where b.country_fk = 75 \
+\
+union all \
+ \
+select brand_name, customer_id, ''  as last_dpst_date, last_log_date, sms_consent, email_consent, email from RND", con = connection)
+
+Entractive_1["email"] = Entractive_1["email"].apply(lambda x: x.decode('utf-8'))
 
 Entractive_1['brand_name'] = ['PLAYDINGO' if x == 'PDIN' \
                                   else x for x in Entractive_1['brand_name']]
@@ -103,14 +150,29 @@ and trx_type = 'DEPOSIT' \
 group by 1 \
 having DATEDIFF(SYSDATE(),last_dpst_date) <= 500 \
 ), \
- \
 30_days_txn_base  as  ( \
 select customer_fk, max(date(c_date)) as 30_dpst_date  from platform.customer_transactions ct \
 where status in ('APPROVED','SUCCESSFUL')  \
 and trx_type = 'DEPOSIT' \
 and DATEDIFF(SYSDATE(),date(c_date)) <= 30 \
-group by 1) \
+group by 1), \
  \
+RND as (select b.name as brand_name, a.id as customer_id, date(a.last_logged_activity_date) as last_log_date, \
+c.accept_marketing_offer_sms as sms_consent, c.accept_marketing_offer as email_consent, a.email, \
+c.referral_info, c.first_name, c.last_name, a.login as user_name, c.phone, c.city,  d.country_iso_code, date(a.c_date) as registration_date \
+from platform.customers as a \
+left join platform.merchants as b \
+on a.merchant_fk = b.id \
+left join platform.customer_attributes as  c \
+on a.id = c.customer_fk \
+left join platform.countries as d \
+on a.country_fk = d.id \
+where a.country_fk in (32) and a.merchant_fk = 77 \
+and DATEDIFF(SYSDATE(),a.c_date) <= 120 and DATEDIFF(SYSDATE(),a.c_date) >  7 \
+and a.id not in (select distinct customer_fk from platform.customer_transactions where status in ('APPROVED','SUCCESSFUL') \
+and trx_type = 'DEPOSIT' ) \
+) \
+\
 select c.name as brand_name, a.customer_fk as customer_id, a.referral_info, a.first_name, a.last_name, \
 b.login as user_name, email, a.phone,  city, d.country_iso_code, date(b.c_date) as registration_date, last_dpst_date, \
 a.accept_marketing_offer as email_consent, a.accept_marketing_offer_sms as sms_consent  \
@@ -126,7 +188,17 @@ on e.customer_fk = a.customer_fk \
 left join 30_days_txn_base  as f \
 on a.customer_fk = f.customer_fk \
 where d.id = 32 \
-and f.30_dpst_date is null and b.merchant_fk = 77", con = connection)
+and f.30_dpst_date is null and b.merchant_fk = 77 \
+ \
+union all \
+ \
+select brand_name, customer_id,referral_info,first_name,last_name, user_name, \
+email, phone, city, country_iso_code, registration_date, \
+''  as last_dpst_date, email_consent,sms_consent from RND", con = connection)
+
+Entractive_2_BR["email"] = Entractive_2_BR["email"].apply(lambda x: x.decode('utf-8'))
+
+Entractive_2_BR["user_name"] = Entractive_2_BR["user_name"].apply(lambda x: x.decode('utf-8'))
 
 Entractive_2_BR['brand_name'] = ['PLAYDINGO' if x == 'PDIN' \
                                   else x for x in Entractive_2_BR['brand_name']]
@@ -148,9 +220,22 @@ select customer_fk, max(date(c_date)) as last_dpst_date  from platform.customer_
 where status in ('APPROVED','SUCCESSFUL') \
 and trx_type = 'DEPOSIT' \
 group by 1 \
-having DATEDIFF(SYSDATE(),last_dpst_date) <= 500 ) \
+having DATEDIFF(SYSDATE(),last_dpst_date) <= 500 ), \
  \
-select c.name as brand_name, a.customer_fk as customer_id, d.last_dpst_date, date(b.last_logged_activity_date) as last_log_date, \
+RND as (select b.name as brand_name, a.id as customer_id, date(a.last_logged_activity_date) as last_log_date, \
+c.accept_marketing_offer_sms as sms_consent, c.accept_marketing_offer as email_consent, a.email \
+from platform.customers as a \
+left join platform.merchants as b \
+on a.merchant_fk = b.id \
+left join platform.customer_attributes as  c \
+on a.id = c.customer_fk \
+where a.country_fk in (32) and a.merchant_fk = 77 \
+and DATEDIFF(SYSDATE(),a.c_date) <= 120 and DATEDIFF(SYSDATE(),a.c_date) >  7 \
+and a.id not in (select distinct customer_fk from platform.customer_transactions where status in ('APPROVED','SUCCESSFUL') \
+and trx_type = 'DEPOSIT' ) \
+) \
+ \
+select c.name as brand_name, a.customer_fk as customer_id, d.last_dpst_date, date(b.last_logged_activity_date) as last_log_date,  \
 a.accept_marketing_offer_sms as sms_consent, a.accept_marketing_offer as email_consent, b.email \
 from  platform.customer_attributes as a \
 left join platform.customers  as b \
@@ -159,7 +244,13 @@ left join platform.merchants as c \
 on b.merchant_fk = c.id \
 inner join txn_base as d \
 on a.customer_fk = d.customer_fk \
-where b.country_fk = 32 and b.merchant_fk = 77", con = connection)
+where b.country_fk = 32 and b.merchant_fk = 77 \
+ \
+union all \
+ \
+select brand_name, customer_id, ''  as last_dpst_date, last_log_date, sms_consent, email_consent, email from RND", con = connection)
+
+Entractive_1_BR["email"] = Entractive_1_BR["email"].apply(lambda x: x.decode('utf-8'))
 
 Entractive_1_BR['brand_name'] = ['PLAYDINGO' if x == 'PDIN' \
                                   else x for x in Entractive_1_BR['brand_name']]
